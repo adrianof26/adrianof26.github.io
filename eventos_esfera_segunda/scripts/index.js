aacc6b89fe9081f2ceec94cdec0db75eb02565ef
Adrián Lorenzo Bueno <130776599+adrianof26@users.noreply.github.com>
2024-01-29 20:49:04 +0100

    Update index.js

--- eventos_esfera_segunda/scripts/index.js
+++ eventos_esfera_segunda/scripts/index.js
@@ -1,38 +1,45 @@
 $(document).ready(function(){
   
-  $('.menu__trigger').click(function(){
-    var that = $(this);
+    // Función para manejar el click en el botón de la hamburguesa
+    $('.menu__trigger').click(function(){
+      var that = $(this);
+      
+      // Mostrar/ocultar el menú
+      $('.menu').slideToggle(500);
+      
+      // Cambiar el fondo del icono de la hamburguesa
+      if (that.hasClass('closed')) {
+        $(".menu__trigger span").css({background: '#FFED4A'});
+        that.removeClass('closed');
+        that.addClass('open');
+      } else {
+        $(".menu__trigger span").css({background: '#ffffff'});
+        that.removeClass('open');
+        that.addClass('closed');
+      }
+    });
     
-    $('.menu').slideToggle(500);
-    $('.menu').css('display', 'grid');
+    // Manejar cambios en el tamaño de la ventana
+    $(window).resize(function(){
+      if($(window).width() > 768) {
+        // Si la ventana es más grande que 768 píxeles, mostrar el menú
+        $('.menu').removeAttr('style');
+      }
+    });
     
-    if (that.hasClass('closed')) {
-      $(".menu__trigger span").css({background: '#FFED4A'});
-      that.removeClass('closed');
-      that.addClass('open');
+    // Parallax
+    $(window).scroll(function() {
+      var windowscroll = $(this).scrollTop();
+      
+      $(".header__bg").css({
+        "transform" : "translate(0%," + windowscroll /15 + "%"
+      });
+    });
+    
+    // Ocultar el menú en dispositivos móviles al cargar la página
+    if ($(window).width() <= 768) {
+      $('.menu').hide();
     }
-    else {
-      $(".menu__trigger span").css({background: '#ffffff'});
-      that.removeClass('open');
-      that.addClass('closed');
-    }
+    
   });
   
-  $(window).resize(function(){
-    if($(window).width() > 768) {
-      $('.menu').removeAttr('style');
-    }
-  });
-  
-});
-
-/*parallax*/
-
-$(window).scroll(function() {
-  var windowscroll = $(this).scrollTop();
-  
-  $(".header__bg").css({
-    "transform" : "translate(0%," + windowscroll /15 + "%"
-  });
-  
-});
